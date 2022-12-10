dd07e272c105aa3a63a791d6f97a761def7d815c
kyle <nzl22@mails.tsinghua.edu.cn>
2022-12-10 15:49:32 +0800

    Add training code for ImageNet.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Deep Model Assembling
 
-This repository will be used to release the code and the pre-trained models for the Deep Model Assembling.
+This repository contains the official code for [Deep Model Assembling](https://arxiv.org/abs/2212.04129).
 
 <p align="center">
     <img src="imgs/teaser.png" width= "450">
@@ -12,13 +12,145 @@ This repository will be used to release the code and the pre-trained models for 
 > **Publish**:&nbsp;&nbsp;&nbsp;*arXiv preprint ([arXiv 2212.04129](https://arxiv.org/abs/2212.04129))*  
 > **Contact**:&nbsp;&nbsp;nzl22 at mails dot tsinghua dot edu dot cn
 
+## News
+
+- `Dec 10, 2022`: release code for training ViT-B, ViT-L and ViT-H on ImageNet-1K.
+
 ## Overview
 
-In this paper, we present a divide-and-conquer strategy for training large models. Our algorithm, *Model Assembling*, divides a large model into smaller modules, optimizes them independently, and then assemble them to obtain the final model. Though conceptually simple, our method significantly outperforms end-to-end (E2E) training in terms of both training efficiency and final accuracy. For example, on ViT-H, Model Assembling outperforms E2E training by **2.7%**, while reducing the training cost by **43%**.
+In this paper, we present a divide-and-conquer strategy for training large models. Our algorithm, Model Assembling, divides a large model into smaller modules, optimizes them independently, and then assembles them together. Though conceptually simple, our method significantly outperforms end-to-end (E2E) training in terms of both training efficiency and final accuracy. For example, on ViT-H, Model Assembling outperforms E2E training by **2.7%**, while reducing the training cost by **43%**.
 
 <p align="center">
     <img src="imgs/ours.png" width= "900">
 </p>
+
+## Data Preparation
+
+- The ImageNet dataset should be prepared as follows:
+
+```
+data
+├── train
+│   ├── folder 1 (class 1)
+│   ├── folder 2 (class 1)
+│   ├── ...
+├── val
+│   ├── folder 1 (class 1)
+│   ├── folder 2 (class 1)
+│   ├── ...
+
+```
+
+## Training on ImageNet-1K
+
+- You can add `--use_amp 1` to train in PyTorch's Automatic Mixed Precision (AMP).
+- Auto-resuming is enabled by default, i.e., the training script will automatically resume from the latest ckpt in <code>output_dir</code>.
+- The effective batch size = `NGPUS` * `batch_size` * `update_freq`. We keep using an effective batch size of 2048. To avoid OOM issues, you may adjust these arguments accordingly.
+- We provide single-node training scripts for simplicity. For multi-node training, simply modify the training scripts accordingly with torchrun:
+
+```bash
+python -m torch.distributed.launch --nproc_per_node=${NGPUS} --master_port=23346 --use_env main.py ...
+
+# modify the above code to
+
+torchrun \
+--nnodes=$NODES \
+--nproc_per_node=$NGPUS \
+--rdzv_backend=c10d \
+--rdzv_endpoint=$MASTER_ADDR:60900 \
+main.py ...
+```
+
+<details>
+<summary>Pre-training meta models (click to expand).</summary>
+
+```bash
+PHASE=PT  # Pre-training
+MODEL=base  # for base
+# MODEL=large  # for large
+# MODEL=huge  # for huge
+NGPUS=8
+
+args=(
+--phase ${PHASE} 
+--model vit_${MODEL}_patch16_224   # for base, large
+# --model vit_${MODEL}_patch14_224   # for huge
+--divided_depths 1 1 1 1 
+--output_dir ./log_dir/${PHASE}/${MODEL}
+
+--batch_size 256
+--epochs 300 
+--drop-path 0 
+)
+
+python -m torch.distributed.launch --nproc_per_node=${NGPUS} --master_port=23346 --use_env main.py "${args[@]}"
+```
+
+</details>
+
+<details>
+<summary>Modular training (click to expand).</summary>
+
+```bash
+PHASE=MT  # Modular Training
+MODEL=base DEPTH=12  # for base
+# MODEL=large DEPTH=24  # for large
+# MODEL=huge DEPTH=32  # for huge
+NGPUS=8
+
+args=(
+--phase ${PHASE} 
+--model vit_${MODEL}_patch16_224   # for base, large
+# --model vit_${MODEL}_patch14_224   # for huge
+--meta_model ./log_dir/PT_${MODEL}/finished_checkpoint.pth  # loading the pre-trained meta model
+
+--batch_size 128
+--update_freq 2
+--epochs 100 
+--drop-path 0.1
+)
+
+# Modular training each target module. Each line can be executed in parallel.
+python -m torch.distributed.launch --nproc_per_node=${NGPUS} --master_port=23346 --use_env main.py "${args[@]}" --idx 0 --divided_depths $((DEPTH/4)) 1 1 1 --output_dir ./log_dir/${PHASE}_${MODEL}_0
+python -m torch.distributed.launch --nproc_per_node=${NGPUS} --master_port=23346 --use_env main.py "${args[@]}" --idx 1 --divided_depths 1 $((DEPTH/4)) 1 1 --output_dir ./log_dir/${PHASE}_${MODEL}_1
+python -m torch.distributed.launch --nproc_per_node=${NGPUS} --master_port=23346 --use_env main.py "${args[@]}" --idx 2 --divided_depths 1 1 $((DEPTH/4)) 1 --output_dir ./log_dir/${PHASE}_${MODEL}_2
+python -m torch.distributed.launch --nproc_per_node=${NGPUS} --master_port=23346 --use_env main.py "${args[@]}" --idx 3 --divided_depths 1 1 1 $((DEPTH/4)) --output_dir ./log_dir/${PHASE}_${MODEL}_3
+
+```
+
+</details>
+
+<details>
+<summary>Assemble & Fine-tuning (click to expand).</summary>
+
+```bash
+PHASE=FT  # Assemble & Fine-tuning
+MODEL=base DEPTH=12  # for base
+# MODEL=large DEPTH=24  # for large
+# MODEL=huge DEPTH=32  # for huge
+NGPUS=8
+
+args=(
+--phase ${PHASE} 
+--model vit_${MODEL}_patch16_224   # for base, large
+# --model vit_${MODEL}_patch14_224   # for huge
+--incubation_models ./log_dir/MT_${MODEL}_*/finished_checkpoint.pth  # for assembling
+--divided_depths $((DEPTH/4)) $((DEPTH/4)) $((DEPTH/4)) $((DEPTH/4)) \
+--output_dir ./log_dir/${PHASE}_${MODEL}
+
+--batch_size 64
+--update_freq 4
+--epochs 100 
+--warmup-epochs 0
+--drop-path 0.1  # for base
+# --drop-path 0.5  # for large
+# --drop-path 0.6  # for huge
+)
+
+python -m torch.distributed.launch --nproc_per_node=${NGPUS} --master_port=23346 --use_env main.py "${args[@]}
+```
+
+</details>
 
 ## Results
 
@@ -50,7 +182,27 @@ In this paper, we present a divide-and-conquer strategy for training large model
 
 ### Data Efficiency
 
-
 <p align="center">
     <img src="./imgs/data_efficiency.png" width= "450">
 </p>
+
+## Citation
+
+If you find our work helpful, please **star🌟** this repo and **cite📑** our paper. Thanks for your support!
+
+```
+@article{Ni2022Assemb,
+  title={Deep Model Assembling},
+  author={Ni, Zanlin and Wang, Yulin and Yu, Jiangwei and Jiang, Haojun and Cao, Yue and Huang, Gao},
+  journal={arXiv preprint arXiv:2212.04129},
+  year={2022}
+}
+```
+
+## Acknowledgements
+
+Our implementation is mainly based on [deit](https://github.com/facebookresearch/deit). We thank to their clean codebase.
+
+## Contact
+
+If you have any questions or concerns, please send mail to [nzl22@mails.tsinghua.edu.cn](mailto:nzl22@mails.tsinghua.edu.cn).
---
+++ utils.py
@@ -0,0 +1,353 @@
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+"""
+Misc functions, including distributed helpers.
+
+Mostly copy-paste from torchvision references.
+"""
+import io
+import os
+import time
+from collections import defaultdict, deque
+import datetime
+from glob import glob
+from pathlib import Path
+
+from torch._six import inf
+
+import torch
+import torch.distributed as dist
+
+
+class NativeScaler:
+    state_dict_key = "amp_scaler"
+
+    def __init__(self):
+        self._scaler = torch.cuda.amp.GradScaler()
+
+    def __call__(self, loss, optimizer, clip_grad=None, parameters=None, create_graph=False, update_grad=True):
+        self._scaler.scale(loss).backward(create_graph=create_graph)
+        if update_grad:
+            if clip_grad is not None:
+                assert parameters is not None
+                self._scaler.unscale_(optimizer)  # unscale the gradients of optimizer's assigned params in-place
+                norm = torch.nn.utils.clip_grad_norm_(parameters, clip_grad)
+            else:
+                self._scaler.unscale_(optimizer)
+                norm = get_grad_norm_(parameters)
+            self._scaler.step(optimizer)
+            self._scaler.update()
+        else:
+            norm = None
+        return norm
+
+    def state_dict(self):
+        return self._scaler.state_dict()
+
+    def load_state_dict(self, state_dict):
+        self._scaler.load_state_dict(state_dict)
+
+
+class SmoothedValue(object):
+    """Track a series of values and provide access to smoothed values over a
+    window or the global series average.
+    """
+
+    def __init__(self, window_size=20, fmt=None):
+        if fmt is None:
+            fmt = "{median:.4f} ({global_avg:.4f})"
+        self.deque = deque(maxlen=window_size)
+        self.total = 0.0
+        self.count = 0
+        self.fmt = fmt
+
+    def update(self, value, n=1):
+        self.deque.append(value)
+        self.count += n
+        self.total += value * n
+
+    def synchronize_between_processes(self):
+        """
+        Warning: does not synchronize the deque!
+        """
+        if not is_dist_avail_and_initialized():
+            return
+        t = torch.tensor([self.count, self.total], dtype=torch.float64, device='cuda')
+        dist.barrier()
+        dist.all_reduce(t)
+        t = t.tolist()
+        self.count = int(t[0])
+        self.total = t[1]
+
+    @property
+    def median(self):
+        d = torch.tensor(list(self.deque))
+        return d.median().item()
+
+    @property
+    def avg(self):
+        d = torch.tensor(list(self.deque), dtype=torch.float32)
+        return d.mean().item()
+
+    @property
+    def global_avg(self):
+        return self.total / self.count
+
+    @property
+    def max(self):
+        return max(self.deque)
+
+    @property
+    def value(self):
+        return self.deque[-1]
+
+    def __str__(self):
+        return self.fmt.format(
+            median=self.median,
+            avg=self.avg,
+            global_avg=self.global_avg,
+            max=self.max,
+            value=self.value)
+
+
+class MetricLogger(object):
+    def __init__(self, delimiter="\t"):
+        self.meters = defaultdict(SmoothedValue)
+        self.delimiter = delimiter
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            assert isinstance(v, (float, int))
+            self.meters[k].update(v)
+
+    def __getattr__(self, attr):
+        if attr in self.meters:
+            return self.meters[attr]
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError("'{}' object has no attribute '{}'".format(
+            type(self).__name__, attr))
+
+    def __str__(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append(
+                "{}: {}".format(name, str(meter))
+            )
+        return self.delimiter.join(loss_str)
+
+    def synchronize_between_processes(self):
+        for meter in self.meters.values():
+            meter.synchronize_between_processes()
+
+    def add_meter(self, name, meter):
+        self.meters[name] = meter
+
+    def log_every(self, iterable, print_freq, header=None):
+        i = 0
+        if not header:
+            header = ''
+        start_time = time.time()
+        end = time.time()
+        iter_time = SmoothedValue(fmt='{avg:.4f}')
+        data_time = SmoothedValue(fmt='{avg:.4f}')
+        space_fmt = ':' + str(len(str(len(iterable)))) + 'd'
+        log_msg = [
+            header,
+            '[{0' + space_fmt + '}/{1}]',
+            'eta: {eta}',
+            '{meters}',
+            'time: {time}',
+            'data: {data}'
+        ]
+        if torch.cuda.is_available():
+            log_msg.append('max mem: {memory:.0f}')
+        log_msg = self.delimiter.join(log_msg)
+        MB = 1024.0 * 1024.0
+        for obj in iterable:
+            data_time.update(time.time() - end)
+            yield obj
+            iter_time.update(time.time() - end)
+            if i % print_freq == 0 or i == len(iterable) - 1:
+                eta_seconds = iter_time.global_avg * (len(iterable) - i)
+                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+                if torch.cuda.is_available():
+                    print(log_msg.format(
+                        i, len(iterable), eta=eta_string,
+                        meters=str(self),
+                        time=str(iter_time), data=str(data_time),
+                        memory=torch.cuda.max_memory_allocated() / MB))
+                else:
+                    print(log_msg.format(
+                        i, len(iterable), eta=eta_string,
+                        meters=str(self),
+                        time=str(iter_time), data=str(data_time)))
+            i += 1
+            end = time.time()
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print('{} Total time: {} ({:.4f} s / it)'.format(
+            header, total_time_str, total_time / len(iterable)))
+
+
+def get_grad_norm_(parameters, norm_type: float = 2.0) -> torch.Tensor:
+    if isinstance(parameters, torch.Tensor):
+        parameters = [parameters]
+    parameters = [p for p in parameters if p.grad is not None]
+    norm_type = float(norm_type)
+    if len(parameters) == 0:
+        return torch.tensor(0.)
+    device = parameters[0].grad.device
+    if norm_type == inf:
+        total_norm = max(p.grad.detach().abs().max().to(device) for p in parameters)
+    else:
+        total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), norm_type).to(device) for p in parameters]),
+                                norm_type)
+    return total_norm
+
+
+def _load_checkpoint_for_ema(model_ema, checkpoint):
+    """
+    Workaround for ModelEma._load_checkpoint to accept an already-loaded object
+    """
+    mem_file = io.BytesIO()
+    torch.save(checkpoint, mem_file)
+    mem_file.seek(0)
+    model_ema._load_checkpoint(mem_file)
+
+
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    import builtins as __builtin__
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop('force', False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
+
+    __builtin__.print = print
+
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+    return dist.get_rank()
+
+
+def is_main_process():
+    return get_rank() == 0
+
+
+def save_on_master(*args, **kwargs):
+    if is_main_process():
+        torch.save(*args, **kwargs)
+
+
+def init_distributed_mode(args):
+    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+        args.rank = int(os.environ["RANK"])
+        args.world_size = int(os.environ['WORLD_SIZE'])
+        args.gpu = int(os.environ['LOCAL_RANK'])
+    elif 'SLURM_PROCID' in os.environ:
+        args.rank = int(os.environ['SLURM_PROCID'])
+        args.gpu = args.rank % torch.cuda.device_count()
+    else:
+        print('Not using distributed mode')
+        args.distributed = False
+        return
+
+    args.distributed = True
+
+    torch.cuda.set_device(args.gpu)
+    args.dist_backend = 'nccl'
+    print('| distributed init (rank {}): {}'.format(
+        args.rank, args.dist_url), flush=True)
+    torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                         world_size=args.world_size, rank=args.rank)
+    torch.distributed.barrier()
+    setup_for_distributed(args.rank == 0)
+
+
+def save_model(args, epoch, model_without_ddp, optimizer, loss_scaler, lr_scheduler, best=False,
+               finished=False):
+    output_dir = Path(args.output_dir)
+
+    if finished:
+        if is_main_process():
+            print('Saving finished checkpoint...')
+        checkpoint_paths = [output_dir / 'finished_checkpoint.pth']
+    elif best:
+        if is_main_process():
+            print('Saving best checkpoint...')
+        checkpoint_paths = [output_dir / 'best_checkpoint.pth']
+    else:
+        epoch_name = str(epoch)
+        checkpoint_paths = [output_dir / ('checkpoint-%s.pth' % epoch_name)]
+
+    for checkpoint_path in checkpoint_paths:
+        to_save = {
+            'model': model_without_ddp.state_dict(),
+            'model_stages': [module.state_dict() for module in model_without_ddp.stages] if hasattr(
+                model_without_ddp, 'model_stages') else [],
+            'optimizer': optimizer.state_dict(),
+            'lr_scheduler': lr_scheduler.state_dict(),
+            'epoch': epoch,
+            'scaler': loss_scaler.state_dict(),
+        }
+
+        save_on_master(to_save, checkpoint_path)
+
+    if is_main_process() and isinstance(epoch, int):
+        to_del = epoch - args.save_ckpt_num * args.save_freq
+        old_ckpt = output_dir / ('checkpoint-%s.pth' % to_del)
+        if os.path.exists(old_ckpt):
+            os.remove(old_ckpt)
+
+
+def auto_load_model(args, model_without_ddp, optimizer, loss_scaler, lr_scheduler):
+    output_dir = Path(args.output_dir)
+    if (output_dir / 'finished_checkpoint.pth').exists():
+        print('Already finished, exiting....')
+        exit(0)
+    checkpoint = None
+    all_checkpoints = sorted([i for i in output_dir.glob('checkpoint-*.pth')
+                              if str(i).split('-')[-1].split('.')[0].isdigit()])[::-1]
+    for ckpt_path in all_checkpoints:
+        try:
+            print(f'Try loading ckpt from {ckpt_path}')
+            checkpoint = torch.load(ckpt_path, map_location='cpu')
+            break
+        except:
+            if is_main_process():
+                print(f'{ckpt_path} cannot be loaded, try next')
+
+    if checkpoint is not None:
+        model_without_ddp.load_state_dict(checkpoint['model'])
+        if is_main_process():
+            print(f'Resume from {ckpt_path} (epoch: {checkpoint["epoch"]})')
+        if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+            args.start_epoch = checkpoint['epoch'] + 1
+            if 'scaler' in checkpoint:
+                loss_scaler.load_state_dict(checkpoint['scaler'])
+
